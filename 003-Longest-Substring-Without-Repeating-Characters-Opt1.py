782c961b02bb256186bf95561e133819bb94cd68
jackcoder <luckjackson2@163.com>
2016-11-22 21:46:42 +0800

    LongestSubstring Optimization1

---
+++ 003-Longest-Substring-Without-Repeating-Characters-Opt1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        strLen = len(s)
+        charSet = set()
+        ans, i, j = 0, 0, 0
+        while i < strLen and j <strLen:
+            if s[j] not in charSet:
+                charSet.add(s[j])
+                j += 1
+                ans = max(ans, j - i)
+            else:
+                charSet.remove(s[i])
+                i += 1
+        return ans
+
+if __name__ == '__main__':
+    result = Solution().lengthOfLongestSubstring('a')
+    print result
+
+
+
+
